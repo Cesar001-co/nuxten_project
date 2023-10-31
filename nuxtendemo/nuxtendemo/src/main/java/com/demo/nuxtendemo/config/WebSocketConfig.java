8bda2c48a0ac68661e9d1b760bfa5dcd74cab3de
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2023-10-31 11:18:47 -0500

    evaluacion

---
+++ nuxtendemo/nuxtendemo/src/main/java/com/demo/nuxtendemo/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.demo.nuxtendemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    public void configureMessegeBroker(MessageBrokerRegistry registry) {
+       registry.enableSimpleBroker("/topic");
+       registry.setApplicationDestinationPrefixes("/ws");
+    }
+
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/nux-websocket").setAllowedOrigins("http://localhost:4200","http://localhost:4200/").withSockJS();
+    }
+}
---
+++ nuxtendemo/nuxtendemo/src/main/java/com/demo/nuxtendemo/services/WebSocketService.java
@@ -0,0 +1,2 @@
+package com.demo.nuxtendemo.services;public class WebSocketService {
+}
