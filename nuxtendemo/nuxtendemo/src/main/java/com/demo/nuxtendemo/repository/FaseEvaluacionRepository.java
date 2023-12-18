c2472a092d44b798177a863f6a4d9a3c8caa5867
Leider Hernandez <lshernandez@unimayor.edu.co>
2023-12-17 21:24:54 -0500

    "Se eliminan clases y entidades sin uso"

--- nuxtendemo/nuxtendemo/src/main/java/com/demo/nuxtendemo/repository/FaseEvaluacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.nuxtendemo.repository;
-
-import com.demo.nuxtendemo.entitys.FaseEvaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaseEvaluacionRepository extends JpaRepository<FaseEvaEntity, Long> {
-}
