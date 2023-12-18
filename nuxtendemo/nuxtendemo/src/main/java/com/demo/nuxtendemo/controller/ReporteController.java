7dd6c1611035b904da0a31706249750e017d82d0
Leider Hernandez <lshernandez@unimayor.edu.co>
2023-12-17 21:27:22 -0500

    "solucion imports"

--- nuxtendemo/nuxtendemo/src/main/java/com/demo/nuxtendemo/controller/ReporteController.java
+++ nuxtendemo/nuxtendemo/src/main/java/com/demo/nuxtendemo/controller/ReporteController.java
@@ -5,6 +5,7 @@ import com.demo.nuxtendemo.services.ReporteServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
